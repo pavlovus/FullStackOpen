fd03737355907a1697e43cb39e7fc3ca04475982
pavlovus <pavlovus7002@gmail.com>
2026-07-16 13:55:19 +0300

    feat(phonebook): complete exercises 3.7-3.8, add logging using morgan middleware

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let contacts = [
     { 
