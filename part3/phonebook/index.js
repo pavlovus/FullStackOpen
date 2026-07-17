04c7bd9cabe1c68b8c677163b49209d022848148
pavlovus <pavlovus7002@gmail.com>
2026-07-17 23:31:04 +0300

    feat(phonebook): complete exercises 3.9-3.11, connect frontend and backend of phonebook, deploy to Render

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,6 +6,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let contacts = [
     { 
@@ -81,7 +82,7 @@ app.post('/api/persons', (request, response) => {
     response.status(201).json(newContact)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
