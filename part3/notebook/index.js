ed1fcb19a0f329cbf931dec962d0a57d24e688d9
pavlovus <pavlovus7002@gmail.com>
2026-07-17 14:39:47 +0300

    feat(notebook): add production build of frontend

--- part3/notebook/index.js
+++ part3/notebook/index.js
@@ -13,6 +13,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -31,10 +32,6 @@ let notes = [
     important: true
   }
 ]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
